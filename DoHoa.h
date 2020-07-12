e5f1b1ba06ce32ad988a6943ff42dc19a2c17720
thienlydochanh <52105481+thienlydochanh@users.noreply.github.com>
2020-07-12 13:46:40 +0700

    Update DoHoa.h

--- DoHoa.h
+++ DoHoa.h
@@ -1,5 +1,285 @@
-#pragma once
-//abc
+#include <winbgim.h> 
+const int H= 700;
+const int W=1360;
+int MID[1350][680];
+// khong duoc xoa neu doc gia da co muon
+// chi tiet 
+void khoitaoID()
+{
+	for(int i=10;i<1350;i++)
+	{
+		for(int j=10;j<680;j++)
+		MID[i][j]=0;
+	}
+}
+void taoID(int x1,int y1,int x2,int y2, int ID)
+{
+	for(int i=x1;i<x2;i++)
+	{
+		for(int j=y1;j<y2;j++)
+		MID[i][j]=ID;
+	}
+}
+const int ENTER = 13;
+const int BACKSPACE = 8;
+
+void ScannerString(char s[],int max, int px ,int py,int ID,int &IDnext)
+{
+	setcolor(0);
+	setbkcolor(15);
+	settextstyle(1,0,1);
+	int mx=-1;
+	int my=-1;
+	unsigned int l = strlen(s);
+		s[l] ='|';
+	outtextxy(px,py,s);
+	while(1)
+	{
+		getmouseclick(WM_LBUTTONDOWN,mx,my);
+		clearmouseclick(WM_LBUTTONDOWN);
+		delay(1);
+		if  (!(((mx==-1)&&(my==-1))||(MID[my][mx]== ID))) {
+			s[l] = ' ';
+			outtextxy(px,py,s);
+			s[l]= '\0';	
+			break;
+		}
+		if(kbhit())
+		{			
+		char c = getch();
+			if(l<max)
+			{
+				if((l==0||s[l-1]==' '))
+				{
+					if(c<='z'&&c>='a')
+					c=c+'A'-'a';
+					else
+					c=c;
+					
+				}
+				else if(c<='Z'&&c>='A')
+				c=c-'A'+'a';
+				if(c=='_')
+				{
+					s[l] = c;
+					l++;
+				}
+				if(c>='a' && c<='z')
+				{
+					s[l] = c;
+					l++;
+				}
+				if(c>='A' && c<='Z')
+				{
+				s[l] = c;
+					l++;
+				}
+				if(c>='0' && c<='9')
+				{
+					s[l] = c;
+					l++;
+				}
+				if(l>0) if(s[l-1]!=' ' && c == ' ')
+				{
+					s[l] = c;
+					l++;
+				}
+				if((l==0||s[l-1]==' ')&&(s[l]>='a'||s[l]<='z'))
+				s[l]=s[l]+'A'-'a';
+				
+				outtextxy(px,py,s);	
+			}
+			if(c == ENTER) {
+				s[l] = ' ';
+				outtextxy(px,py,s);
+				s[l]= '\0';
+				break;
+			}
+			if(c == BACKSPACE&&l>0)
+			{
+				s[l]=' ';
+				outtextxy(px,py,s);	
+				s[l-1] =' ';
+				l--;
+			}
+			s[l] = '|';
+			outtextxy(px,py,s);	
+			if(s[l]==' ') s[l]=0;
+		
+		}
+	}
+	if((mx!=-1)&&(my!=-1)) {
+	IDnext=MID[mx][my];
+	}
+}
+
+void ScannerName(char s[],int max, int px ,int py,int ID, int &IDnext)
+{
+	setcolor(0);
+	setbkcolor(15);
+	settextstyle(1,0,1);
+	int mx=-1;
+	int my=-1;
+	unsigned int l = strlen(s);
+		s[l] ='|';
+	outtextxy(px,py,s);
+	while(1)
+	{
+		getmouseclick(WM_LBUTTONDOWN,mx,my);
+		clearmouseclick(WM_LBUTTONDOWN);
+		delay(1);
+		if  (!(((mx==-1)&&(my==-1))||(MID[my][mx]== ID))) {
+			s[l] = ' ';
+			outtextxy(px,py,s);
+			s[l]= '\0';	
+			break;
+		}
+		if(kbhit())
+		{			
+		char c = getch();
+			if(l<max)
+			{
+
+				if((l==0||s[l-1]==' '))
+				{
+					if(c<='z'&&c>='a')
+					c=c+'A'-'a';
+					else
+					c=c;
+					
+				}
+				else if(c<='Z'&&c>='A')
+				c=c-'A'+'a';
+
+				if(c>='a' && c<='z')
+				{
+					s[l] = c;
+					l++;
+				}
+				if(c>='A' && c<='Z')
+				{
+				s[l] = c;
+					l++;
+				}
+				
+				if(l>0) if(s[l-1]!=' ' && c == ' ')
+				{
+					s[l] = c;
+					l++;
+				}
+				
+			}
+			if(c == ENTER) {
+				s[l] = ' ';
+				outtextxy(px,py,s);
+				s[l]= '\0';
+				break;
+			}
+			if(c == BACKSPACE&&l>0)
+			{
+				s[l]=' ';
+				outtextxy(px,py,s);
+				s[l-1] =' ';
+				l--;
+			}
+			s[l] = '|';
+			outtextxy(px,py,s);	
+			if(s[l]==' ') s[l]='\0';
+		
+		}
+	}
+	if((mx!=-1)&&(my!=-1)) {
+	IDnext=MID[mx][my];
+	}
+}
+
+int ScannerNum(char s[], int max, int px ,int py,int ID, int &IDnext)
+{
+	setcolor(0);
+	setbkcolor(15);
+	settextstyle(1,0,1);
+
+	int mx=-1;
+	int my=-1;
+	unsigned int l = strlen(s);
+	s[l] ='|';
+	outtextxy(px,py,s);
+	while(1)
+	{
+		getmouseclick(WM_LBUTTONDOWN,mx,my);
+		clearmouseclick(WM_LBUTTONDOWN);
+		delay(1);
+		if  (!(((mx==-1)&&(my==-1))||(MID[my][mx]== ID))) {
+			
+			s[l] = ' ';
+			outtextxy(px,py,s);
+			s[l]= '\0';	
+			break;
+		}
+		if(kbhit())
+		{
+				
+			char c = getch();
+			
+			if(l<max)
+			{
+				
+				if(c>='0' && c<='9')
+				{
+					s[l] = c;
+				 outtextxy(px,py,s);
+					l++;
+				}
+			}
+			if(c == ENTER) {
+				s[l] = ' ';
+				outtextxy(px,py,s);
+				s[l]= '\0';
+				break;
+			}
+			if(c == BACKSPACE&&l>0){
+				s[l]=' ';
+				outtextxy(px,py,s);
+				s[l-1] =' ';
+			
+				l--;
+			}
+			s[l] = '|';
+			outtextxy(px,py,s);	
+			if(s[l]==' ') s[l]='\0';
+		}
+	}
+	if((mx!=-1)&&(my!=-1)) 
+	IDnext=MID[mx][my];
+}
+void mancanhbao()
+{
+    setfillstyle(1,11);	
+	bar(20,465,300,675);
+	setbkcolor(11);
+	setcolor(12);
+	settextstyle(0,0,2);
+	outtextxy(100,470,"CANH BAO");
+	
+}
+void khungChinh()
+{
+	setfillstyle(1,11);// man
+	bar(0,0,1360,700);
+	setcolor(0);
+    rectangle(2,2,1353,693);
+    rectangle(4,4,273,270);
+    rectangle(4,275,273,690);
+    rectangle(280,4,1350,220);
+    rectangle(280,225,1350,690);
+
+	setbkcolor(11);
+	setcolor(12);
+	settextstyle(0,0,2);
+	outtextxy(90,285,"CANH BAO");	
+    
+}
+
 void button(int x1, int y1, int x2, int y2, int ID, char* s,int cochu, int clbar, int clw)
 {
 	setfillstyle(1,clbar);
@@ -55,3 +335,80 @@ void taobang(int x1,int y1, int docao,int socot,int Mangdodai[], int mau, int ma
 		x1=x1+Mangdodai[i];
 	}	
 }
+void taobang1(int x1,int y1, int docao,int socot,int Mangdodai[], int mau, int mauvien,int soluong, int ID, int trang,char td[][50] )
+{
+	setcolor(0);
+	setbkcolor(mau);
+	settextstyle(0,0,2);
+	int y=y1;
+	for(int i=1;i<=soluong;i++)
+	{
+		taodong(x1,y1,docao,Mangdodai,mau, mauvien,ID+i,socot);
+		y1=y1+docao;
+		
+	}
+	for(int i=0;i<socot;i++)
+	{
+		outtextxy(x1+5,y+5,td[i]);
+		x1=x1+Mangdodai[i];
+	}	
+}
+void danhdau(int vt)
+{
+	setcolor(12);
+	setbkcolor(15);
+	settextstyle(0,0,2);
+	int y1=245+35;
+	for(int i=1;i<=10;i++)
+	{
+		
+	    outtextxy(1300,y1," ");
+		y1=y1+32;
+		
+		
+	}
+	outtextxy(1300,245+35+(vt-1)*32,"v");
+		
+}
+void giaodiendau()
+{
+	khungChinh();
+	button(6,30,270,90,1,"QUAN LY LOP HOC", 2,1,15);
+	button(6,100,270,160,2,"QUAN LY LOP TC", 2,1,15);
+	button(6,170,270,230,3,"QUAN LY MON HOC", 2,1,15);
+	
+}
+void giaodiennhapthongtinqllh()
+{
+	button(280,4,420,36,11,"Lop Hoc",2,1,15);
+	button(425,4,590,36,11,"Sinh Vien",2,1,15);
+	button(595,4,695,36,11,"Diem",2,1,15);
+	
+}
+void bangthongtinqllh()
+{
+	
+}
+
+void Home(){
+	setbkcolor(11);
+	setcolor(COLOR(247,145,31));                   
+    settextstyle(4,0,5);
+    outtextxy(500,100,"LIBRARY MANAGER");
+ 	settextstyle(4,0,3);
+ 	settextjustify(0,0);  
+ 	outtextxy(670,325,"Tac gia:");
+ 	outtextxy(695,370,"PTIT HCM ");
+ 	outtextxy(695,420,"Ho Va ten - MaSinhVien");
+ 	setlinestyle(1,2,5);
+ 	line(555, 312, 660, 312);
+ 	line(807, 312, 1200, 312);
+ 	line(695, 450, 1300, 450);
+ 	setcolor(COLOR(255,0,0)); 
+ 	setbkcolor(11);
+	setcolor(12);
+	settextjustify(0,2);
+	settextstyle(0,0,2);
+	setlinestyle(0,1,1);
+}
+

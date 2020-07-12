875e63a98b653ff79dd8fe93faa1741826940cb0
thienlydochanh <52105481+thienlydochanh@users.noreply.github.com>
2020-07-12 13:46:53 +0700

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,16 +1,19 @@
 #include <winbgim.h> 
 
 #include "LopTinChi.h"
+#include "DoHoa.h"
 
 int main(int argc, char *argv[])
 {
 	// now, you can run project
-	initwindow(300, 300);			// init window graphics
+	initwindow(1365, 770);			// init window graphics
 	setbkcolor(1);					// set background
    	cleardevice();
 	setcolor(14);					// set text color
 	outtextxy(50,100,"Graphics in Dev-C++");// print text in window graphics
-	
+//	khungChinh();
+	giaodiendau();
+	giaodiennhapthongtinqllh();
 	while(!kbhit()) delay(1);		// pause screen	
 	return 0;
 }
